5f7a6935b83b6a34344de1487d7b3d1f2244823f
devpixelchoi <85727063+devpixelchoi@users.noreply.github.com>
2021-06-19 23:33:47 +0900

    21~27

--- 21_30_Quiz.py
+++ 21_30_Quiz.py
@@ -3,13 +3,32 @@ letters = 'python'
 print(letters[0],letters[2])
 
 # Q-022
-
+license_plate = "24A 2210"
+print(license_plate[4:])
+print(license_plate[-4:])
 
 # Q-023
+strings = "OEOEOEOE"
+print(strings[::2])
+print(strings[::3])
+
 # Q-024
+string = "PYTHON"
+print(string[::-1])
+
 # Q-025
+phone_number = "010-1111-2222"
+re_number = phone_number.replace("-", " ")
+print(re_number)
+
 # Q-026
+number_2 = phone_number.replace("-", "")
+print(number_2)
+
 # Q-027
+url = "http://sharebook.kr"
+print(url[-2:])
+
 # Q-028
 # Q-029
 # Q-030
